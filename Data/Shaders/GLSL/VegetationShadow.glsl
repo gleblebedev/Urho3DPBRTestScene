cbd49b30e252467eb03a0b31d96fef335b622dc9
Gleb Lebedev <gleb@gleblebedev.com>
2020-07-13 20:17:13 +0100

    Vegetation fixed

--- Data/Shaders/GLSL/VegetationShadow.glsl
+++ Data/Shaders/GLSL/VegetationShadow.glsl
@@ -5,6 +5,9 @@ uniform float cWindHeightFactor;
 uniform float cWindHeightPivot;
 uniform float cWindPeriod;
 uniform vec2 cWindWorldSpacing;
+#ifdef WINDSTEMAXIS
+    uniform vec3 cWindStemAxis;
+#endif
 
 #ifdef VSM_SHADOW
     varying vec4 vTexCoord;
@@ -17,8 +20,12 @@ void VS()
     mat4 modelMatrix = iModelMatrix;
     vec3 worldPos = GetWorldPos(modelMatrix);
     
-    vec3 relativePos = iPos.xyz * GetNormalMatrix(modelMatrix);
-    float windStrength = max(relativePos.y - cWindHeightPivot, 0.0) * cWindHeightFactor;
+    #ifdef WINDSTEMAXIS
+        float stemDistance = dot(iPos.xyz, cWindStemAxis);
+    #else
+        float stemDistance = iPos.y;
+    #endif
+    float windStrength = max(stemDistance - cWindHeightPivot, 0.0) * cWindHeightFactor;
     float windPeriod = cElapsedTime * cWindPeriod + dot(worldPos.xz, cWindWorldSpacing);
     worldPos.x += windStrength * sin(windPeriod);
     worldPos.z -= windStrength * cos(windPeriod);
